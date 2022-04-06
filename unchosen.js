193e0182a9d59c551a9f4990e0a79d497677d91c
BranJaLu <brandon.lu@student.cart.org>
2022-04-06 13:47:51 -0700

    commit

--- unchosen.js
+++ unchosen.js
@@ -1,1 +1,15 @@
-var terminator = "I'm Back"
+let player = {
+    name: "Player", //Name of the player will be used in the story.
+    playing: 0, //0 means that the game is ongoing an other numbers will bring up different endings.
+    choices: [] //This gives the player to pick important choices on their adventure.
+}
+
+var story = {
+prolog: "In a world where steampunk and fantasy exist in one world, SPAF. It was a normal day in a rural village where you are doing your usual business."
+
+
+}
+//test
+
+var action = [];
+var choice = [];
