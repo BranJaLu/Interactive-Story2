cd378447cf05ac101acfeba3cfc90518aed1a4b4
Alex Hernandez <94017152+AlexHernandez001@users.noreply.github.com>
2022-04-29 15:15:02 -0700

    in progress

--- unchosen.js
+++ unchosen.js
@@ -27,3 +27,77 @@ var story = {
         text: "The group offers to train the hero in a specific skill to prepare them for the fight."
         options: [["magic", "learn magic"], ["sword", "learn to swordfight"], ["gun", "learn to use a gun"]]
     }
+    Magic: {
+        text: "You always wanted to learn how to use magic. While a bit strict, she is the most empathetic and also the youngest of the group. But this learning of magic is very complex and you will have to learn the basics for 8 months. Hope that you are prepared to use it properly."
+    }
+
+    Firearm: {
+         text: "Wanting to be the hack and slash person, you asked the boastful expert that you are interested in that type of art. It’ll take a maximum of 6 months to complete."
+    }
+
+    Sword: {
+        text: "These skills seem too complex so you decided to take the path that is much easier. Plus, the dwarf has a much more fun personality than the other two mentors. Your training under his hands will last for 5 months."
+    }
+
+   Test: {
+       text: ""
+       options: [[""],[""],[""]]
+   } 
+
+   Hoard: {
+       text: ""
+       options: [[""],[""],[""]]
+   }
+
+   Hole: {
+       text: ""
+   }
+
+   Proceed1: {
+       text: ""
+       options: [[""],[""],[""]]
+   }
+
+   Tunnel: {
+       text: ""
+       options: [[""],[""],[""]] 
+   }
+
+   Enter: {
+       text: "Well this is better than this.” You started before running into it. The tunnel then closes behind you and now you’re all alone."
+   }
+
+   Artifict: {
+       text: ""
+       options: [[""],[""],[""]]
+   }
+
+   Hellers: {
+       text:""
+       options: [[""],[""],[""]]
+   }
+
+Proceed2: {
+    text: ""
+    options: [["Investigate"]]
+}
+
+LeaveCave: {
+    text: ""
+    options: [[""],[""],[""]]
+}
+
+Boss: {
+    text: ""
+    options: [[""],[""],[""],[""]]
+}
+
+Ending: {
+    text: ""
+    options: [[""],[""],[""]]
+}
+
+
+
+
+
